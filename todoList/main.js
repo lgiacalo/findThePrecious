b17b22e9f8de5463f1cf9ca8e1faff4d3a8987ba
lgiacalo <lgiacalo@student.42.fr>
2020-09-24 13:18:55 +0200

    page.js with cdn ok

--- todoList/main.js
+++ todoList/main.js
@@ -2,7 +2,6 @@
 
 const buttonChecks = document.querySelectorAll(".button-check")
 
-
 for (let btnCheck of buttonChecks){
     btnCheck.addEventListener('click', (e) => {
         e.target.classList.toggle("far");
@@ -11,3 +10,32 @@ for (let btnCheck of buttonChecks){
         e.target.classList.toggle("fa-check-circle");
     })
 }
+
+
+page('/', index);
+page('/about', about);
+page('/items', contact);
+page('/contact/:contactName', contact);
+page();
+
+function index(e) {
+  document.querySelector('p')
+    .textContent = 'viewing index';
+  console.log('e :>> ', e);
+}
+
+function about(e) {
+  console.log('e :>> ', e);
+
+  document.querySelector('p')
+    .textContent = 'viewing about';
+}
+
+function contact(ctx) {
+  console.log('ctx :>> ', ctx);
+
+  document.querySelector('p')
+    .textContent = 'viewing contact ' + (ctx.params.contactName || '');
+}
+
+
